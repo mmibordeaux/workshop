316392a3960beab79f1303e2a73c3ad2bc3be9b7
Kora Saccharin <kora.saccharin@mailoo.org>
2016-01-05 14:37:46 +0100

    Style new session and new registration screens

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -11,6 +11,7 @@
 
 class ProjectsController < ApplicationController
   before_action :set_project, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /projects
   def index
