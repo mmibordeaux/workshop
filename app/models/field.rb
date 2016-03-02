2d8489a99c718c16707a1899094ae4563c48746a
Kora Saccharin <kora.saccharin@mailoo.org>
2016-03-02 22:35:49 +0100

    Add error message

--- app/models/field.rb
+++ app/models/field.rb
@@ -14,6 +14,10 @@ class Field < ActiveRecord::Base
   has_many :features
   default_scope { order('position') }
 
+  validates :name, presence: true
+  validates :color, presence: true
+  validates :position, presence: true
+
   def to_s
     "#{name}"
   end
